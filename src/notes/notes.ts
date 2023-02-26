5fd68693e3af4218abccb9eaa5c5ce1b55e307f9
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-02-26 15:26:25 +0200

    Delete notes files

--- src/notes/notes.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import TheNotes from "./TheNotes.vue";
-
-import "../assets/main.css";
-import "mdb-vue-ui-kit/css/mdb.min.css";
-
-createApp(TheNotes).mount("#app");
