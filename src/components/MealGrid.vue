980f8bf5f9ec66cbfff6ca691ec5105de82f9ff2
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-03-05 19:06:50 +0200

    Refactor script setup for meal grid component

--- src/components/MealGrid.vue
+++ src/components/MealGrid.vue
@@ -1,6 +1,20 @@
-<script lang="ts">
+<script setup lang="ts">
 import AnImageTile from "./AnImageTile.vue";
+</script>
 
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div v-for="(item, index) in items" :key="index" class="col-auto">
+        <router-link :to="{ name: 'page', params: { id: item.id } }"
+          ><AnImageTile :img_path="item.img_path" :title="item.fullTitle"
+        /></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
 export default {
   components: {
     AnImageTile,
@@ -29,15 +43,3 @@ export default {
   },
 };
 </script>
-
-<template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div v-for="(item, index) in items" :key="index" class="col-auto">
-        <router-link :to="{ name: 'page', params: { id: item.id } }"
-          ><AnImageTile :img_path="item.img_path" :title="item.fullTitle"
-        /></router-link>
-      </div>
-    </div>
-  </div>
-</template>
