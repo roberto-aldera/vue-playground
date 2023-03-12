4bea1f3912dfa04a982432d1f05c320834a5ff67
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-03-12 21:35:10 +0200

    Add separate files for custom interfaces

---
+++ src/components/CategoryObj.ts
@@ -0,0 +1,7 @@
+import type { ItemObj } from "./ItemObj";
+
+export interface CategoryObj {
+  name: string;
+  items: ItemObj[];
+  img_path: string;
+}
---
+++ src/components/ItemObj.ts
@@ -0,0 +1,5 @@
+export interface ItemObj {
+  id: string;
+  fullName: string;
+  img_path: string;
+}
