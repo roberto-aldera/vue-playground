d06eeb17ffd639eda53812a9bd4c1a24b45a01e5
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-04-16 21:13:35 +0200

    Revert "Edit path"
    This reverts commit 034f82660e11178c66e05caccc35d72570920b7c.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./vue-playground/",
+  base: "/vue-playground/",
   plugins: [vue(), vueJsx()],
   css: {
     devSourcemap: true,
