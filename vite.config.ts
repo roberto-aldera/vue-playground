afef752c5843d128e658103c54b9a721706f2aea
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-04-16 21:02:52 +0200

    Another path change...

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/vue-playground/",
   plugins: [vue(), vueJsx()],
   css: {
     devSourcemap: true,
